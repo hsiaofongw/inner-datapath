cbca220d18f6efac94f53ea83b93a929eb8f3289
root <root@nyc1.exploro.one>
2025-03-09 12:29:12 +0000

    added launch script

---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+wgIfName=vrf-blue-wg0
+vrfName=vrf-blue
+vRouteTable=42
+
+ip link add "$vrfName" type vrf table "$vRouteTable"
+ip link add "$wgIfName" type wireguard
+ip link set "$wgIfName" master "$vrfName"
+
+listenPort=$(cat data/$(hostname)/listenport)
+wg set "$wgIfName" listen-port $listenPort
+wg set "$wgIfName" private-key data/$(hostname)/.private/privkey
+
+for f in data/*; do
+  ipCidr=$(cat $f/ipcidr)
+  if [ "$(hostname)" = $(basename $f) ]; then
+    continue
+  fi
+
+  peerHost=$(cat $f/clearnet)
+  peerPort=$(cat $f/listenport)
+  peerPubkey=$(cat $f/pubkey)
+  peerEndpoint=$peerHost:$peerPort
+  
+
+  ip=$(echo $ipCidr | awk -F'/' '{print $1}')
+  allowedIps=$(echo $ip/32)
+
+  wg set "$wgIfName" peer "$peerPubkey" endpoint "$peerEndpoint" allowed-ips $allowedIps
+done
+
+ipcidr=$(cat data/$(hostname)/ipcidr)
+ip addr add $ipcidr dev "$wgIfName"
