85fd240e7fe311b78878146357ee1e90353b102f
Ubuntu <ubuntu@frank1.exploro.one>
2025-03-12 20:59:41 +0800

    update routereflector script

--- connect_route_reflector.sh
+++ connect_route_reflector.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+set -e
+
 scriptPath=$(realpath $0)
 scriptDir=$(dirname $scriptPath)
 vmHostName=routereflector
 wgIfName=routereflector-wg0
+
+netnsKey=$(docker inspect $vmHostName --format '{{.NetworkSettings.SandboxKey}}')
+if [ -z "$netnsKey" ]; then
+  echo "Can't get netns key."
+  exit 1
+fi
+
+ip link add $wgIfName type wireguard
