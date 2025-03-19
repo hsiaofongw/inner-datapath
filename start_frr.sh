737ec0698369b6a9106066fe81077a3fdc781709
Ubuntu <ubuntu@frank1.exploro.one>
2025-03-20 04:31:57 +0800

    refactor, delete redundant code

--- start_frr.sh
+++ start_frr.sh
@@ -3,12 +3,19 @@
 scriptPath=$(realpath $0)
 scriptDir=$(dirname $scriptPath)
 
+syshostname=$(hostname)
+hostname=${HOST:-"$syshostname"}
+echo hostname: $hostname
+
+defaultCont=frr
+cont=${CONTAINER:-"$defaultCont"}
+echo containername: $cont
+
 docker run \
   --privileged \
   --rm \
   -it \
-  --name frr \
-  -v $scriptDir/data/$(hostname)/frr/etc:/etc/frr \
+  --name $cont \
+  --hostname $hostname \
+  -v $scriptDir/data/$hostname/frr/etc:/etc/frr \
   quay.io/frrouting/frr:10.2.1 
-
-# --entrypoint /bin/bash \
--- start_route_reflector.sh
+++ start_route_reflector.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
+set -e
+
 scriptPath=$(realpath $0)
 scriptDir=$(dirname $scriptPath)
-vmHostName=routereflector
 
-docker run \
-  --privileged \
-  --rm \
-  -it \
-  --name $vmHostName \
-  --hostname $vmHostName \
-  -v $scriptDir/data/$vmHostName/frr/etc:/etc/frr \
-  -v $scriptDir/data:/root/data \
-  quay.io/frrouting/frr:10.2.1
-
-# --entrypoint /bin/bash \
-# $scriptDir/connect_route_reflector.sh
-# docker exec -dit $vmHostName /usr/lib/frr/docker-start
+HOST=routereflector CONTAINER=routereflector $scriptDir/start_frr.sh
