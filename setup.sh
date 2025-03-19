550707f0093f6f56caecde7ce99d2cdd59e79a44
Ubuntu <ubuntu@frank1.exploro.one>
2025-03-19 21:38:01 +0800

    update address

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 wgIfName=vrf-blue-wg0
-# vrfName=vrf-blue
 vRouteTable=42
 
-# ip link add "$vrfName" type vrf table "$vRouteTable"
 ip link add "$wgIfName" type wireguard
-# ip link set "$wgIfName" master "$vrfName"
 ip link set "$wgIfName" up
 
 listenPort=$(cat data/$(hostname)/listenport)
@@ -26,11 +23,11 @@ for f in data/*; do
   
 
   ip=$(echo $ipCidr | awk -F'/' '{print $1}')
-  allowedIp=$(echo $ip/32)
+  allowedIp=$(echo $ip/128)
 
   wg set "$wgIfName" peer "$peerPubkey" endpoint "$peerEndpoint" allowed-ips $allowedIp
 done
 
 ipcidr=$(cat data/$(hostname)/ipcidr)
 ip addr add $ipcidr dev "$wgIfName"
-# ip link set "$vrfName" up
+
--- unset.sh
+++ unset.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 wgIfName=vrf-blue-wg0
-vrfName=vrf-blue
 vRouteTable=42
 
 ip route flush table 42
 ip link del "$wgIfName" 
-ip link del "$vrfName"
+
