bb69fbc210d0e017d31c5094dd6bbb49462ff620
Ubuntu <ubuntu@frank1.exploro.one>
2025-03-20 13:06:28 +0800

    update script

--- pingmany.sh
+++ pingmany.sh
@@ -5,7 +5,7 @@ for f in data/*; do
   echo "ping host: $host"
   while read -r line; do
     ip=$(echo $line | cut -d '/' -f1)
-    if [[ "$ip" == fe80:* ]]; then
+    if [[ "$ip" =~ fe80:* ]]; then
       ip --json a show to fe80::/64 | jq -r '.[] | .ifname' | while read -r ifname; do
          echo interface: $ifname
 	 ping -n -c1 $ip%$ifname
--- setup_vxlan.sh
+++ setup_vxlan.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-# vrfName=${VRFNAME:-"vrf-blue"}
+scriptPath=$(realpath $0)
+scriptDir=$(dirname $scriptPath)
+
 vni=${VNI:-"42"}
 vxlanDstPort=${VXDSTPORT:-"4789"}
 vxlanIfName=${VXIFNAME:-"vxlan42"}
@@ -14,13 +16,24 @@ if [ -z "$hostName" ]; then
   exit 1
 fi
 
-localIp=$(cat data/$hostName/ipcidr | cut -d '/' -f1)
+localIp=""
+localDev=""
+while read -r ipcidr; do
+  if [[ "$ipcidr" =~ fe80:* ]]; then
+    continue
+  fi
+  localIp=$(echo $ipcidr | cut -d '/' -f1)
+  localDev=$(ip --json a show to $localIp | jq -r '.[] | .ifname')
+  break
+done < $scriptDir/data/$hostName/ipcidr
+
 if [ -z "$localIp" ]; then
-  echo "No local IP found."
+  echo "No local ip found."
   exit 1
 fi
 
-ip link add "$vxlanIfName" type vxlan id "$vni" dstport "$vxlanDstPort" local "$localIp" nolearning
+# specify dev btw to let it work out correct MTU automatically
+ip link add "$vxlanIfName" type vxlan id "$vni" dev "$localDev" dstport "$vxlanDstPort" local "$localIp" nolearning
 
 ip link add "$brIfName" type bridge
 ip link set "$vxlanIfName" master "$brIfName"
