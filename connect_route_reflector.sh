d8c241c6828d1dc24db0cbd566659da5811278ef
root <root@ashburn1.exploro.one>
2025-04-01 06:27:25 +0100

    added new routereflector

--- connect_route_reflector.sh
+++ connect_route_reflector.sh
@@ -5,4 +5,23 @@ set -e
 scriptPath=$(realpath $0)
 scriptDir=$(dirname $scriptPath)
 
-HOST=routereflector CONTAINER=routereflector $scriptDir/setup_wg.sh
+thisHost=$(hostname)
+
+found=0
+for confD in $scriptDir/data/*; do
+  basename=$(basename $confD)
+  if [ -d "$confD" ] && [[ "$basename" =~ routereflector* ]]; then
+    clearnet=$(cat $confD/clearnet)
+    if [ "$clearnet" = "$thisHost" ]; then
+      echo found RouteReflector: $basename
+      found=$((found+1))
+      HOST=$basename CONTAINER=$basename $scriptDir/setup_wg.sh
+    fi
+  fi
+done
+
+if [ "$found" = "0" ]; then
+  echo "No RouteReflector found."
+  exit 1
+fi
+
